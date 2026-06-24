eaf773677dfc48d8963f1116b02cb48e023528e1
bryannwww <bryanpdeossa@gmail.com>
2026-06-24 17:30:12 -0500

    CAMBIOS 23/06/2026 Se realizo el landing del admin al igual que si Base

--- menu/views.py
+++ menu/views.py
@@ -4,17 +4,10 @@ from django.shortcuts import render, redirect
 def ver_carta(request):
     return render(request, "menu/carta.html")
 
-def ver_carta(request):
-    # 1. Por defecto, asumimos que es un cliente normal
-    plantilla_base = 'base.html'
-    
-    # 2. Si hay sesión iniciada y el rol es 'admin', cambiamos la plantilla
-    if request.session.get('logueado', {}).get('rol') == 'admin':
-        plantilla_base = 'base_admin.html'
-    
-    # Aquí puedes tener lógica extra si traes los platos de la base de datos...
-    
-    # 3. Pasamos 'base_template' dentro del diccionario de contexto
-    return render(request, 'nombre_de_tu_html_de_la_carta.html', {
-        'base_template': plantilla_base
-    })
+def carta_admin(request):
+    return render(request, "menu/menu_admin.html")
+
+def crear_plato(request):
+    if request.method == "POST":
+        pass
+
